a2ca209059ebc02443cce02c1e1c563390d913a5
zaynram <ramdasszayn@gmail.com>
2025-09-29 20:18:36 -0500

    propgating

--- scripts/server-cmd.py
+++ scripts/server-cmd.py
@@ -4,15 +4,17 @@ from rampy import root, sh
 
 
 def main(*script_cmds: str) -> None:
-    session_string = f"({str(root.join('scripts', 'server-session.ps1', resolve=True))})"
-    scriptblock_string = f"{{{'\n\r'.join(script_cmds)}}}"
+    commands = ["cd ~/share/n8n", *script_cmds]
     sh.logs.enable()
-    sh.pwsh(f"icm -session {session_string} -scriptblock {scriptblock_string}")
+    sh.pwsh(
+        *commands,
+        remote_session=str(root.join("scripts", "server-session.ps1", resolve=True)),
+    )
     sh.logs.disable()
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument(type=str, nargs="+", dest="commands")
+    parser.add_argument(type=str, nargs="*", dest="commands")
     args = parser.parse_args()
     main(*args.commands)
