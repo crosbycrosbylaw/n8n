69e21f7656556b8a64b9720c8b30f5494c5a4244
zaynram <ramdasszayn@gmail.com>
2025-09-30 23:23:28 -0400

    renaming

--- scripts/serve-n8n.ps1
+++
@@ -1,5 +0,0 @@
-if (get-command 'node.exe' -erroraction silentlycontinue) {
-    [string]$serve_script = join-path $psscriptroot 'serve-n8n.mjs'
-    [string[]]$arguments = @($serve_script, $args[0], $args[1])
-    start-process -filepath node.exe -argumentlist $arguments -nonewwindow -wait -passthru | out-null
-} else { write-error 'node.exe is not on PATH' }
