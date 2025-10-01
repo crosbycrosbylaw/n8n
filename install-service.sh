ad08da25ef2161bc7aa8b9834af12583c668ee3d
zaynram <ramdasszayn@gmail.com>
2025-09-30 20:30:53 -0500

    service done

--- install-service.sh
+++ install-service.sh
@@ -48,8 +48,10 @@ create_user() {
 install_files() {
     print_status "${BLUE}" "Installing files to ${INSTALL_DIR}"
 
-		ln -s "$SCRIPT_DIR" "$INSTALL_DIR"
-		mkdir -p "$INSTALL_DIR/.n8n"
+		if [ ! -e "$INSTALL_DIR" ]; then
+				ln -s "$SCRIPT_DIR" "$INSTALL_DIR"
+				mkdir -p "$INSTALL_DIR/.n8n"
+		fi
 
     # Set ownership and permissions
     chown -R "${SERVICE_USER}:${SERVICE_GROUP}" "${INSTALL_DIR}"
