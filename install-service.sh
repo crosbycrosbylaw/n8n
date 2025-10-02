3ea92c494a600ce3ecfe15e68149791239a1f61b
zaynram <ramdasszayn@gmail.com>
2025-10-01 20:49:35 -0500

    autocommit

--- install-service.sh
+++ install-service.sh
@@ -48,10 +48,12 @@ create_user() {
 install_files() {
     print_status "${BLUE}" "Installing files to ${INSTALL_DIR}"
 
-	if [ ! -e "$INSTALL_DIR" ]; then
-			ln -s "$SCRIPT_DIR" "$INSTALL_DIR"
-			mkdir -p "$INSTALL_DIR/.n8n"
-	fi
+		if [ ! -e "$INSTALL_DIR" ]; then
+				ln -s "$SCRIPT_DIR" "$INSTALL_DIR"
+		fi
+
+		mkdir -p "$INSTALL_DIR/.n8n" >/dev/null 2>&1
+		mkdir -p "$INSTALL_DIR/logs" >/dev/null 2>&1
 
     # Set ownership and permissions
     chown -R "${SERVICE_USER}:${SERVICE_GROUP}" "${INSTALL_DIR}"
