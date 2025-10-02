e8903ccb1591665d9682ca785171d894bdff0eb7
zaynram <ramdasszayn@gmail.com>
2025-10-01 20:46:07 -0500

    autocommit

--- serve.sh
+++ serve.sh
@@ -16,6 +16,8 @@ function printerror {
 }
 
 [ -s "$HOME/.bun/bin/bun" ] || printerror 'could not find bun'
+export PATH="$HOME/.bun/bin/bun:$PATH"
+
 [ -s "/usr/bin/node" ] || printerror 'could not find node'
 
 if [ -z "$1" ] || [ "$1" == "start" ]
