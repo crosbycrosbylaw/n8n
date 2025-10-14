776cded27419162d33f157d4a72224bfc5e73355
zaynram <ramdasszayn@gmail.com>
2025-10-14 16:17:30 -0500

    auto

--- scripts/service.ps1
+++ scripts/service.ps1
@@ -1,17 +1,41 @@
-﻿
+﻿[CmdletBinding()]
+param(
+  [parameter(mandatory)]
+  [validateset('start', 'stop', 'reload', 'status')]
+  $action
+)
+
 $n8n = @{
   name = 'n8n.service'
   path = (get-command 'pixi').path
   args = 'run n8n'
   root = (join-path $psscriptroot '..')
-  proc = $null
 }
 
-function start-n8n {
-  return start-process -filepath $n8n.path $n8n.args `
-    -workingdirectory $n8n.root -windowstyle hidden -passthru
+$scripts = @{
+  start  = {
+    start-job -name $n8n.name -scriptblock {
+      $process = $null
+      while ($true) { if (!$process -or $process.hasexited) {
+          $process = (start-process -filepath $n8n.path $n8n.args `
+              -workingdirectory $n8n.root -windowstyle hidden -passthru)
+        }
+      }
+    }
+  }
+  stop   = {
+    $job = get-job $n8n.name -erroraction silentlycontinue
+    if ($job) { $job.stopjob(); $job | remove-job }
+  }
+  reload = {
+    & $scripts.stop
+    & $scripts.start
+  }
 }
 
-start-job -name 'n8n.service' -scriptblock {
-  while ($true) { if (!$n8n.proc -or $n8n.proc.hasexited) { $n8n.proc = start-n8n } }
+switch ($action) {
+  'start' { & $scripts.start }
+  'stop' { & $scripts.stop }
+  'reload' { & $scripts.reload }
+  'status' { get-job $n8n.name }
 }
