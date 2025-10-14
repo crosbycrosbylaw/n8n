72c4437cb183d89369ab9ebc9e8df9de349714b8
zaynram <ramdasszayn@gmail.com>
2025-10-14 15:50:40 -0500

    auto

--- scripts/service.ps1
+++ scripts/service.ps1
@@ -1,8 +1,15 @@
-﻿$n8n = @{
+﻿
+$n8n = @{
   name = 'n8n.service'
   path = (get-command 'pixi').path
   args = 'run n8n'
   root = (join-path $psscriptroot '..')
+  proc = $null
 }
 
-start-process -filepath $n8n.path $n8n.args -workingdirectory $n8n.root -nonewwindow
+function start-n8n {
+  return start-process -filepath $n8n.path $n8n.args `
+    -workingdirectory $n8n.root -windowstyle hidden -passthru
+}
+
+while ($true) { if (!$n8n.proc -or $n8n.proc.hasexited) { $n8n.proc = start-n8n } }
