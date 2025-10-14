eb9dc944c6437eaf765188be90037cb0e85c8111
zaynram <ramdasszayn@gmail.com>
2025-10-14 14:55:32 -0500

    changes

--- scripts/service.ps1
+++ scripts/service.ps1
@@ -1,14 +1,14 @@
 ﻿[string]$nssm_path = (get-command 'nssm').path
 function invoke-nssm { & $nssm_path @args }
 
-
 $n8n = @{
   name = 'n8n.service'
-  path = (get-command 'node').path
-  args = (join-path $psscriptroot '../server/bin/serve.mjs')
-  root = (join-path $psscriptroot '../server/bin')
+  path = (get-command 'pixi').path
+  args = "run $((get-command 'bun').path) x n8n"
+  root = (join-path $psscriptroot '../service')
 }
 
-invoke-nssm install $n8n.name $n8n.path $n8n.args
+invoke-nssm install $n8n.name $n8n.path
 invoke-nssm set $n8n.name AppDirectory $n8n.root
+invoke-nssm set $n8n.name AppParameters $n8n.args
 invoke-nssm set $n8n.name Start SERVICE_AUTO_START
