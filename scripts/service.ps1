b43dc88680f38b67f208b7ab4bd706d8e6ca5838
zaynram <ramdasszayn@gmail.com>
2025-10-14 15:37:26 -0500

    auto

--- scripts/service.ps1
+++ scripts/service.ps1
@@ -1,14 +1,8 @@
-﻿[string]$nssm_path = (get-command 'nssm').path
-function invoke-nssm { & $nssm_path @args }
-
-$n8n = @{
+﻿$n8n = @{
   name = 'n8n.service'
   path = (get-command 'pixi').path
   args = 'run n8n'
-  root = (join-path $psscriptroot '../service')
+  root = (join-path $psscriptroot '..')
 }
 
-invoke-nssm install $n8n.name $n8n.path
-invoke-nssm set $n8n.name AppDirectory $n8n.root
-invoke-nssm set $n8n.name AppParameters $n8n.args
-invoke-nssm set $n8n.name Start SERVICE_AUTO_START
+start-process -filepath $n8n.path $n8n.args -workingdirectory $n8n.root -nonewwindow
