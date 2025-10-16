7545bb183982d6fbef8abe3c4128cc68021fa5f6
zaynram <ramdasszayn@gmail.com>
2025-10-15 20:34:41 -0500

    auto

--- python/n8n_py/parser/cls.py
+++ python/n8n_py/parser/cls.py
@@ -82,7 +82,7 @@ class ResponseUtility:
         self._require(attachment=True)
 
         if self.disposition:
-            return self.disposition.split("=", 1)[-1].strip().strip('"')
+            return html.unescape(self.disposition.split("=", 1)[-1].strip().strip('"'))
 
         count = len([*TMP.iterdir()])
         extension = "txt" if not self.mime_type else self.mime_type.split("/", 1)[-1]
