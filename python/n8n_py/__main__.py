73fb8e63d664f20e933aec78381320dab23a269d
zaynram <ramdasszayn@gmail.com>
2025-11-07 23:12:38 -0600

    pixi run pwsh (get-date).timeofday

--- python/n8n_py/__main__.py
+++ python/n8n_py/__main__.py
@@ -7,5 +7,13 @@ if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("path", required=True, type=Path)
 
-    args = parser.parse_args()
-    main(args.path)
+    try:
+        args = parser.parse_args()
+        main(args.path)
+    except TypeError:
+        import sys
+
+        if len(sys.argv) > 1:
+            main(Path(sys.argv[1]))
+        else:
+            raise
