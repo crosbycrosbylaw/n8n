52929e82f9c5f1dd9e96b6e923416886512316c4
Zayn Ramdass <zramda2@illinois.edu>
2025-12-06 09:06:39 -0600

    12/6/2025 9:06:38 AM Saturday, December 6, 2025 9:06:39 AM

--- src/automate/eserv/util/doc_store.py
+++ src/automate/eserv/util/doc_store.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import contextlib
 import typing
-from datetime import UTC, datetime, timedelta
 
 if typing.TYPE_CHECKING:
     from pathlib import Path
@@ -13,19 +12,12 @@ from rampy import rootpath
 
 TMP = rootpath('service', 'tmp', mkdir=True)
 
-prune_interval: timedelta = timedelta(hours=6)
-pruned_at: datetime = datetime.now(UTC) - prune_interval
-
 
 def _prune_temp() -> None:
     for p in TMP.iterdir():
         if p.is_dir() and not [*p.iterdir()]:
             with contextlib.suppress(PermissionError):
                 p.unlink()
-
-    global pruned_at  # noqa: PLW0603
-
-    pruned_at = datetime.now(UTC)
 
 
 def _clean_document_name(raw: str | None) -> str:
@@ -35,8 +27,7 @@ def _clean_document_name(raw: str | None) -> str:
 
 def document_store_factory(name: str | None = None) -> Path:
     """Get a temporary directory to store downloaded documents."""
-    if (datetime.now(UTC) - prune_interval) > pruned_at:
-        _prune_temp()
+    _prune_temp()
 
     cleaned = _clean_document_name(name)
 
